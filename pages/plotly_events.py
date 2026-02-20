936343a0a14499626fcacc553e98f05e48ebc0c7
Huyubin <1979578551@qq.com>
2026-02-20 16:45:45 +0800

    add plotly_events.py

---
+++ pages/plotly_events.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from streamlit_plotly_events import plotly_events
+
+st.title('Streamlit Plotly Events Example: Penguins')
+df = pd.read_csv('penguins.csv')
+fig = px.scatter(df, x='bill_length_mm', y='bill_depth_mm',color'species')
+plotly_events(fig)
